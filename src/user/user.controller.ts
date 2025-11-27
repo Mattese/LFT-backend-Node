19e84ba7e7e0d6bfed4b5fdb480b6d900da508f5
Martin Reichl <reichlm@seznam.cz>
2025-11-27 14:15:29 +0100

    update user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,30 +1,29 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
-import { UserDto } from './dtos/user.dto';
-import { Sex } from './enums/sex.enum';
+import {
+  Body,
+  Controller,
+  Get,
+  Header,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { UserService } from './services/user.services';
+import { User } from './entities/user.entity';
+import type CreateUserDto from './dto/user.dto';
 
 @Controller('user')
 export class UserController {
+  constructor(private usersService: UserService) {}
+  @Post()
+  @Header('Content-Type', 'application/json')
+  createUser(@Body() createUserDto: CreateUserDto) {
+    console.log('Creating user:', createUserDto);
+    return 'Creating user endpoint - to be implemented';
+  }
+
   // TODO: Define POST, DELETE, PATCH
   @Get(':id')
-  getById(
-    @Param('id', ParseIntPipe)
-    _id: number,
-  ): UserDto | null {
-    // Calculate a date 20 years ago
-    const date = new Date();
-    date.setFullYear(date.getFullYear() - 20);
-    const dateOfBirth = date.toISOString().split('T')[0];
-
-    // TODO: change to provide real data
-    const userDto = new UserDto(
-      'Jmeno',
-      'Prijmeni',
-      dateOfBirth,
-      'testovaci user',
-      Sex.MALE,
-    );
-
-    const user = userDto.toEntity();
-    return UserDto.from(user);
+  findById(@Param('id', ParseIntPipe) id: number): User | null {
+    return this.usersService.findById(id);
   }
 }

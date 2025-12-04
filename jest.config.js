504e68bd7ac407a513de52b1e9399d9257950baf
Martin Reichl <reichlm@seznam.cz>
2025-12-04 19:46:16 +0100

    add tests

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+export default {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {}],
+  },
+  transformIgnorePatterns: ['node_modules/(?!(@faker-js)/)'],
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+};
